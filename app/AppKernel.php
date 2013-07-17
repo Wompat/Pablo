00908f59bc363cdb65e376acae839a6e5909df36
toofoo <thomasdecraux@gmail.com>
2013-07-17 22:14:00 +0200

    Update 20130717

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,14 +18,12 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Pablo\UserBundle\PabloUserBundle(),
             new Pablo\PeopleBundle\PabloPeopleBundle(),
-            new Pablo\OrganisationBundle\PabloOrganisationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new atoum\AtoumBundle\AtoumAtoumBundle();
         }
 
         return $bundles;
--- src/Pablo/OrganisationBundle/PabloOrganisationBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Pablo\OrganisationBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class PabloOrganisationBundle extends Bundle
-{
-}
